de0f40008c4f49078a1a59c4f52021286de32dad
Saleem9390 <158169867+Saleem9390@users.noreply.github.com>
2024-03-20 10:39:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import news from './components/news';
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+      
+    </div>
+  );
+}
+
+export default news;
---
+++ news.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function news(){
+    return(
+      <><div className='news'>
+        <h2>Time news</h2>
+      </div><div className='listofstories'>
+          <div className='listofstories1'> <a href='https://time.com/6142934/amy-schneider-jeopardy-streak-ends'>Amy Schneider’s Jeopardy! Streak Ends at 40 Consecutive Wins and $1.4 Million</a>
+          </div>
+          <div  className='listofstories2'>
+          <a href='https://time.com/6142628/writing-with-fire-india-documentary'>Writing With Fire' Shines a Light on All-Women News Outlet</a>
+          </div>
+          <div  className='listofstories3'> 
+            <a href='https://time.com/6142628/writing-with-fire-india-documentary'>Writing With Fire' Shines a Light on All-Women News Outlet</a>
+           </div>
+          <div  className='listofstories4'> 
+            <a href='"https://time.com/6142743/joe-biden-supreme-court-nominee-black-woman-campaign-promise'>Pressure Mounts for Biden to Nominate a Black Woman to the Supreme</a>
+             </div>
+          <div  className='listofstories5'>
+            <a href='https://time.com/6142769/james-webb-space-telescope-reaches-l2'>The James Webb Space Telescope Is in Position—And Now We Wait</a></div>
+            </div>
+           <div  className='listofstories6'> 
+            <a href='https://time.com/6142718/we-need-new-national-covid-19-response-plan'>We Urgently Need a New National COVID-19 Response Plan</a>
+            </div>
+        </>
+      
+    )
+}
+export default news
